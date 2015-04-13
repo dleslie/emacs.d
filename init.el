82446a9c17a8cfd81b693c6145a7fc089d1e2a94
Dan Leslie <dan@ironoxide.ca>
2015-04-13 15:21:46 -0700

    Fixed merlin always being loaded

--- init.el
+++ init.el
@@ -600,7 +600,7 @@ directory to make multiple eshell windows easier."
 (when (memq 'ocaml my-optional-init)
   (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
   (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-  (require 'merlin)
+  (autoload 'merlin-mode "Merlin" "Merlin Mode" t)
   
   (setq merlin-use-auto-complete-mode t)
   (setq merlin-command 'opam)
