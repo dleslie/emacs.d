96b014c656625a80fef5b78540df95a9a25eca35
Dan Leslie <dan@ironoxide.ca>
2016-01-06 11:39:48 -0800

    Use official elpy package repo

--- init.d/10-packages.el
+++ init.d/10-packages.el
@@ -8,6 +8,7 @@
 (setq package-archives 
       '(("gnu" . "https://elpa.gnu.org/packages/")
 	("melpa" . "https://melpa.org/packages/")
-        ("org" . "http://orgmode.org/elpa/")))
+        ("org" . "http://orgmode.org/elpa/")
+	("elpy" . "http://jorgenschaefer.github.io/packages/")))
 
 (package-initialize)
