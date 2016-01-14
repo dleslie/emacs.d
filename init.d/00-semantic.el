94613bdf4135792e3febb8d1d84788efe3a2feb3
Dan Leslie <dan@ironoxide.ca>
2016-01-13 22:47:55 -0800

    May as well enable ede

--- init.d/00-semantic.el
+++ init.d/00-semantic.el
@@ -5,7 +5,11 @@
 (message "Configuring Semantic and CEDET")
 
 (require 'semantic)
+(require 'semantic/bovine/el)
 (require 'semantic/bovine/gcc)
+(require 'semantic/bovine/c)
+(require 'semantic/senator)
+
 (require 'srecode)
 (require-package 'srefactor)
 
@@ -17,6 +21,7 @@
 (global-semantic-idle-summary-mode 1)
 (global-semantic-idle-breadcrumbs-mode 1)
 (global-semantic-idle-completions-mode nil)
+(global-ede-mode 1)
 
 (with-eval-after-load "company"
   (add-to-list 'company-backends 'company-semantic))
