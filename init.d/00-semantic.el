097b7368f5f30765bc37b57b4988d3e9567b7ae5
Dan Leslie <dan@ironoxide.ca>
2016-07-05 16:09:26 -0700

    Disabled semantic

--- init.d/00-semantic.el
+++ init.d/00-semantic.el
@@ -2,26 +2,25 @@
 ;; Semantic
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(message "Configuring Semantic and CEDET")
+;; (message "Configuring Semantic and CEDET")
 
-(require 'semantic)
-(require 'semantic/bovine/el)
-(require 'semantic/bovine/gcc)
-(require 'semantic/bovine/c)
-(require 'semantic/senator)
+;; (require 'semantic)
+;; (require 'semantic/ia)
+;; (require 'semantic/bovine/el)
+;; (require 'semantic/bovine/gcc)
+;; (require 'semantic/senator)
+;; (require 'srecode)
 
-(require 'srecode)
+;; (when my-system-include-paths
+;;   (mapc #'(lambda (s) (semantic-add-system-include s)) my-system-include-paths))
 
-(when my-system-include-paths
-  (mapc #'(lambda (s) (semantic-add-system-include s)) my-system-include-paths))
+;; (global-semantic-idle-scheduler-mode 1)
+;; (global-semantic-idle-summary-mode nil)
+;; (global-semantic-idle-breadcrumbs-mode nil)
+;; (global-semantic-idle-completions-mode nil)
+;; (global-semantic-idle-local-symbol-highlight-mode nil)
 
-(global-semantic-idle-scheduler-mode 1)
-(global-semantic-idle-summary-mode nil)
-(global-semantic-idle-breadcrumbs-mode nil)
-(global-semantic-idle-completions-mode nil)
-(global-semantic-idle-local-symbol-highlight-mode nil)
+;; (semantic-mode 1)
 
-;(semantic-mode 1)
-
-(with-eval-after-load "company"
-  (add-to-list 'company-backends 'company-semantic))
+;; (with-eval-after-load "company"
+;;   (add-to-list 'company-backends 'company-semantic))
