5249266b7096c857b59b0096aa6a83f37120096a
Dan Leslie <dan@ironoxide.ca>
2016-07-30 14:45:27 -0700

    Added srefactor

---
+++ init.d/30-semantic-additional.el
@@ -0,0 +1,11 @@
+(require-package 'srefactor)
+
+(require 'srefactor)
+(require 'srefactor-lisp)
+
+(define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+(define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+(global-set-key (kbd "M-RET o") 'srefactor-lisp-one-line)
+(global-set-key (kbd "M-RET m") 'srefactor-lisp-format-sexp)
+(global-set-key (kbd "M-RET d") 'srefactor-lisp-format-defun)
+(global-set-key (kbd "M-RET b") 'srefactor-lisp-format-buffer)
