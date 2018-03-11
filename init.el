e3e5494e97f602177a845e7683457784f33339ea
Dan Leslie <dan@ironoxide.ca>
2018-03-11 11:53:11 -0700

    ...

--- init.el
+++ init.el
@@ -338,12 +338,12 @@ Code taken from `hack-dir-local-variables'."
 
     (defun my-org-save-hook ()
       (when (and (eq major-mode 'org-mode)
-                 (string-match (expand-file-name org-mobile-directory) (expand-file-name buffer-file-name)))
+                 (string-match (file-truename org-mobile-directory) (file-truename buffer-file-name)))
         (org-mobile-push)))
 
     (defun my-org-load-hook ()
       (when (and (eq major-mode 'org-mode)
-                 (string-match (expand-file-name org-mobile-directory) (expand-file-name buffer-file-name)))
+                 (string-match (file-truename org-mobile-directory) (file-truename buffer-file-name)))
         (org-mobile-pull)))
     
     (defun my-custom-org-hook ()
