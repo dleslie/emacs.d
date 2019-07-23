e95486e3c2bcda04bb7c0f82455158787aa373d7
Dan Leslie <dan@ironoxide.ca>
2019-07-22 23:29:51 -0700

    ...

--- init.el
+++ init.el
@@ -240,6 +240,7 @@ Code taken from `hack-dir-local-variables'."
         lsp-ui-flycheck-enable t
         lsp-ui-sideline-enable nil)
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  :config
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
 
