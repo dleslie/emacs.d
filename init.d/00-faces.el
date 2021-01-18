e276b229edc69e9ed2c3159f8f219ca0cba3275f
Dan Leslie <dan@ironoxide.ca>
2021-01-18 11:41:00 -0800

    Fixes face loading in daemon mode

--- init.d/00-faces.el
+++ init.d/00-faces.el
@@ -7,17 +7,28 @@
 
 ;;; Code:
 (defvar default-faces-cache '())
+(defvar after-cache-faces-hook '())
 
-(setq default-faces-cache
-      (mapcar (lambda (face)
-		`(,face .
-			((foreground . ,(face-foreground face))
-			 (background . ,(face-background face))
-			 (stipple . ,(face-stipple face))
-			 (bold . ,(face-bold-p face))
-			 (italic . ,(face-italic-p face))
-			 (underline . ,(face-underline-p face)))))
-	      (face-list)))
+(defun cache-faces ()
+  "Caches all faces."
+  (when (not default-faces-cache)
+    (setq default-faces-cache
+	  (mapcar (lambda (face)
+		    `(,face .
+			    ((foreground . ,(face-foreground face))
+			     (background . ,(face-background face))
+			     (stipple . ,(face-stipple face))
+			     (bold . ,(face-bold-p face))
+			     (italic . ,(face-italic-p face))
+			     (underline . ,(face-underline-p face))
+			     (font . ,(face-font face))
+			     (attributes . ,(face-all-attributes face)))))
+		  (face-list)))
+    (run-hooks after-cache-faces-hook)))
+
+(if (daemonp)
+    (add-hook 'server-after-make-frame-hook #'cache-faces)
+  (cache-faces))
 
 (defun reset-face (face)
   "Reset FACE to what its value was on load, if possible."
@@ -29,12 +40,19 @@
 	      (stipple (cdr (assoc 'stipple (cdr data))))
 	      (bold (cdr (assoc 'bold (cdr data))))
 	      (italic (cdr (assoc 'italic (cdr data))))
-	      (underline (cdr (assoc 'underline (cdr data)))))
+	      (underline (cdr (assoc 'underline (cdr data))))
+	      (font (cdr (assoc 'font (cdr data))))
+	      (attributes (cdr (assoc 'attributes (cdr data)))))
 	  (set-face-foreground face foreground)
 	  (set-face-background face background)
 	  (set-face-stipple face stipple)
 	  (set-face-italic face italic)
-	  (set-face-underline face underline))))))
+	  (set-face-underline face underline)
+	  (set-face-font face font)
+	  (mapc
+	   (lambda (attr)
+	     (set-face-attribute face (car attr) (cdr attr)))
+	   attributes))))))
 
 (defun reset-theme ()
   "Disable all active themes."
--- init.d/99-themes.el
+++ init.d/99-themes.el
@@ -7,23 +7,32 @@
 
 ;;; Code:
 
-(use-package afternoon-theme)
-(use-package alect-themes)
-(use-package ample-theme)
-(use-package clues-theme)
-(use-package color-theme-sanityinc-tomorrow)
-(use-package constant-theme)
-(use-package cyberpunk-theme)
-(use-package flatland-theme)
-(use-package gruber-darker-theme)
-(use-package gruvbox-theme)
-(use-package moe-theme)
-(use-package sexy-monochrome-theme)
-(use-package solarized-theme)
-(use-package zenburn-theme)
+(defun acquire-themes ()
+  "Acquires themes."
+  (let ((active-theme (car custom-enabled-themes)))
+    (use-package afternoon-theme)
+    (use-package alect-themes)
+    (use-package ample-theme)
+    (use-package clues-theme)
+    (use-package color-theme-sanityinc-tomorrow)
+    (use-package constant-theme)
+    (use-package cyberpunk-theme)
+    (use-package flatland-theme)
+    (use-package gruber-darker-theme)
+    (use-package gruvbox-theme)
+    (use-package moe-theme)
+    (use-package sexy-monochrome-theme)
+    (use-package solarized-theme)
+    (use-package zenburn-theme)
+    (reset-theme)
+    (load-theme active-theme t)))
 
-(reset-theme)
-(load-theme 'tango t)
+;; Running Emacs as a daemon doesn't properly configure default faces
+;; until after the first window is created. So we attach a hook to the
+;; cache faces method in order to wait for that to occur.
+(if (daemonp)
+    (add-hook 'after-cache-faces-hook #'acquire-themes)
+  (acquire-themes))
 
 (provide '99-themes)
 ;;; 99-themes.el ends here
