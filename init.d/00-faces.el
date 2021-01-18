a20308a0136abc0e85d0952007d485bc2174de92
Dan Leslie <dan@ironoxide.ca>
2021-01-18 15:23:23 -0800

    Better theme caching

--- init.d/00-faces.el
+++ init.d/00-faces.el
@@ -6,60 +6,11 @@
 ;; dan@ironoxide.ca
 
 ;;; Code:
-(defvar default-faces-cache '())
-(defvar after-cache-faces-hook '())
-
-(defun cache-faces ()
-  "Caches all faces."
-  (when (not default-faces-cache)
-    (setq default-faces-cache
-	  (mapcar (lambda (face)
-		    `(,face .
-			    ((foreground . ,(face-foreground face))
-			     (background . ,(face-background face))
-			     (stipple . ,(face-stipple face))
-			     (bold . ,(face-bold-p face))
-			     (italic . ,(face-italic-p face))
-			     (underline . ,(face-underline-p face))
-			     (font . ,(face-font face))
-			     (attributes . ,(face-all-attributes face)))))
-		  (face-list)))
-    (run-hooks 'after-cache-faces-hook)))
-
-(if (daemonp)
-    (add-hook 'server-after-make-frame-hook 'cache-faces)
-  (cache-faces))
-
-(defun reset-face (face)
-  "Reset FACE to what its value was on load, if possible."
-  (ignore-errors
-    (let ((data (assoc face default-faces-cache)))
-      (when data
-	(let ((foreground (cdr (assoc 'foreground (cdr data))))
-	      (background (cdr (assoc 'background (cdr data))))
-	      (stipple (cdr (assoc 'stipple (cdr data))))
-	      (bold (cdr (assoc 'bold (cdr data))))
-	      (italic (cdr (assoc 'italic (cdr data))))
-	      (underline (cdr (assoc 'underline (cdr data))))
-	      (font (cdr (assoc 'font (cdr data))))
-	      (attributes (cdr (assoc 'attributes (cdr data)))))
-	  (set-face-foreground face foreground)
-	  (set-face-background face background)
-	  (set-face-stipple face stipple)
-	  (set-face-italic face italic)
-	  (set-face-underline face underline)
-	  (set-face-font face font)
-	  (mapc
-	   (lambda (attr)
-	     (set-face-attribute face (car attr) (cdr attr)))
-	   attributes))))))
-
 (defun reset-theme ()
   "Disable all active themes."
   (interactive)
   (while custom-enabled-themes
-    (disable-theme (car custom-enabled-themes)))
-  (mapc #'reset-face (face-list)))
+    (disable-theme (car custom-enabled-themes))))
 
 (defun change-theme (theme)
   "Disable all enabled themes and then load the provided theme THEME."
--- init.d/99-themes.el
+++ init.d/99-themes.el
@@ -7,35 +7,32 @@
 
 ;;; Code:
 
-(defun acquire-themes (&optional immediate)
+(defun acquire-themes (&optional defer)
   "Acquires themes.
 Will not defer loading if IMMEDIATE is true."
   (let ((active-theme (car custom-enabled-themes)))
-    (use-package afternoon-theme :defer (not immediate))
-    (use-package alect-themes :defer (not immediate))
-    (use-package ample-theme :defer (not immediate))
-    (use-package clues-theme :defer (not immediate))
-    (use-package color-theme-sanityinc-tomorrow :defer (not immediate))
-    (use-package constant-theme :defer (not immediate))
-    (use-package cyberpunk-theme :defer (not immediate))
-    (use-package flatland-theme :defer (not immediate))
-    (use-package gruber-darker-theme :defer (not immediate))
-    (use-package gruvbox-theme :defer (not immediate))
-    (use-package moe-theme :defer (not immediate))
-    (use-package sexy-monochrome-theme :defer (not immediate))
-    (use-package solarized-theme :defer (not immediate))
-    (use-package zenburn-theme :defer (not immediate))
+    (use-package afternoon-theme :defer defer)
+    (use-package alect-themes :defer defer)
+    (use-package ample-theme :defer defer)
+    (use-package clues-theme :defer defer)
+    (use-package color-theme-sanityinc-tomorrow :defer defer)
+    (use-package constant-theme :defer defer)
+    (use-package cyberpunk-theme :defer defer)
+    (use-package flatland-theme :defer defer)
+    (use-package gruber-darker-theme :defer defer)
+    (use-package gruvbox-theme :defer defer)
+    (use-package moe-theme :defer defer)
+    (use-package sexy-monochrome-theme :defer defer)
+    (use-package solarized-theme :defer defer)
+    (use-package zenburn-theme :defer defer)
     (when (not (ignore-errors (change-theme active-theme)))
       (change-theme 'tango))))
 
 ;; Running Emacs as a daemon doesn't properly configure default faces
 ;; until after the first window is created. So we attach a hook to the
 ;; cache faces method in order to wait for that to occur.
-(if (daemonp)
-    (add-hook 'after-cache-faces-hook 'acquire-themes)
-  (progn
-    (acquire-themes)
-    (add-hook 'after-init-hook 'acquire-themes)))
+(acquire-themes t)
+(add-hook 'after-init-hook 'acquire-themes)
 
 (provide '99-themes)
 ;;; 99-themes.el ends here
