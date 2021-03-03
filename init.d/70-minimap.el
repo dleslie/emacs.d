6ae0a7acfa97e1386e28c1bc6f4e37e6d28fdbaa
Dan Leslie <dan@ironoxide.ca>
2021-03-02 22:26:26 -0800

    Added minimap

---
+++ init.d/70-minimap.el
@@ -0,0 +1,20 @@
+;;; 70-minimap.el --- minimap
+
+;;; Commentary:
+
+;; Emacs configuration of Dan Leslie.
+;; dan@ironoxide.ca
+
+;;; Code:
+
+(use-package minimap
+  :init
+  (setq minimap-update-delay 0)
+  (setq minimap-window-location 'right)
+  (setq minimap-hide-fringes t)
+  (setq minimap-always-recenter nil)
+  (setq minimap-major-modes '(prog-mode text-mode org-mode))
+  (minimap-mode 1))
+
+(provide '70-minimap)
+;;; 70-minimap.el ends here
