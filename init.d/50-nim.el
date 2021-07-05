6ea1945808ac8d8939bdf4a21056d57a1d57b280
Dan Leslie <dan@ironoxide.ca>
2021-07-05 15:02:52 -0700

    Added nim

---
+++ init.d/50-nim.el
@@ -0,0 +1,17 @@
+;;; 50-nim.el --- nim
+
+;;; Commentary:
+
+;; Emacs configuration of Dan Leslie.
+;; dan@ironoxide.ca
+
+;;; Code:
+
+(use-package nim-mode
+  :ensure t
+  :hook
+  (nim-mode . lsp))
+
+(provide '50-nim)
+;;; 50-nim.el ends here
+-
