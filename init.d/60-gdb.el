91be30426e78e983577d73d5e6e8b22015f6b43d
Dan Leslie <dan@ironoxide.ca>
2021-08-19 10:41:17 -0700

    Minor improvements

--- init.d/60-gdb.el
+++ init.d/60-gdb.el
@@ -8,12 +8,13 @@
 ;;; Code:
 
 ;; Replaces the built-in gdb-mi with something better
-(use-package gdb-mi
-  :after hydra
-  :straight (:host github :repo "weirdNox/emacs-gdb" :files ("*.el" "*.c" "*.h" "Makefile"))
-  :init
-  (fmakunbound 'gdb)
-  (fmakunbound 'gdb-enable-debug))
+(when (executable-find "gdb")
+  (use-package gdb-mi
+    ;;  :after hydra
+    :straight (:host github :repo "weirdNox/emacs-gdb" :files ("*.el" "*.c" "*.h" "Makefile"))
+    :init
+    (fmakunbound 'gdb)
+    (fmakunbound 'gdb-enable-debug)))
 
 (provide '60-gdb)
 ;;; 60-gdb.el ends here
