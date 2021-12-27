e14a2dc6d2840c720e79e1a8003dcb857fbea37f
Dan Leslie <dan@ironoxide.ca>
2021-12-27 14:33:11 -0800

    Fixed org shortcuts

--- init.d/70-org.el
+++ init.d/70-org.el
@@ -12,12 +12,12 @@
 (use-package org
   :ensure org-plus-contrib
   :init
-  (global-set-key "\C-c \C-o t" 'org-todo-list)
-  (global-set-key "\C-c \C-o t" 'org-store-link)
-  (global-set-key "\C-c \C-o t" 'org-agenda)
-  (global-set-key "\C-c \C-o t" 'org-iswitchb)
-  (global-set-key "\C-c \C-o t" 'org-capture)
-  (global-set-key "\C-c \C-o t" 'my-org-show-all-inline-images)
+  (global-set-key "\C-cot" 'org-todo-list)
+  (global-set-key "\C-col" 'org-store-link)
+  (global-set-key "\C-coa" 'org-agenda)
+  (global-set-key "\C-cos" 'org-iswitchb)
+  (global-set-key "\C-coc" 'org-capture)
+  (global-set-key "\C-coi" 'my-org-show-all-inline-images)
   (defun my-org-show-all-inline-images ()
     (interactive)
     (org-display-inline-images t t)))
