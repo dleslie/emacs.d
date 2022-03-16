fa65a2d2ff9df5b6960b479a998416bf36770231
Dan Leslie <dan@ironoxide.ca>
2022-03-16 11:33:16 -0700

    Support dockerfiles

---
+++ init.d/50-docker.el
@@ -0,0 +1,13 @@
+;;; 70-docker.el --- docker
+
+;;; Commentary:
+
+;; Emacs configuration of Dan Leslie.
+;; dan@ironoxide.ca
+
+;;; Code:
+
+(use-package dockerfile-mode)
+
+(provide '70-docker)
+;;; 70-docker.el ends here
