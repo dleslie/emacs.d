bbd9c26ceb17b5d22646438f71e8e06cc18a1d8f
Dan Leslie <dan@ironoxide.ca>
2022-05-07 18:13:29 -0700

    Better LSP loading

--- init.d/30-lsp.el
+++ init.d/30-lsp.el
@@ -38,9 +38,9 @@
   :after lsp-mode
   :init
   (setq lsp-ui-sideline-update-mode 'line
-	lsp-ui-sideline-delay 0.2
-	lsp-ui-imenu-auto-refresh t
-	lsp-ui-sideline-show-code-actions nil)
+				lsp-ui-sideline-delay 0.2
+				lsp-ui-imenu-auto-refresh t
+				lsp-ui-sideline-show-code-actions nil)
   :config
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
@@ -49,28 +49,27 @@
   :after lsp-mode)
 
 (use-package lsp-docker
-  :defer t)
+  :after lsp-mode
+	:init
+	(defvar lsp-docker-client-packages
+		'(lsp-css lsp-clients lsp-bash lsp-go lsp-pyls lsp-html lsp-typescript
+							lsp-terraform lsp-cpp))
 
-(defvar lsp-docker-client-packages
-  '(lsp-css lsp-clients lsp-bash lsp-go lsp-pyls lsp-html lsp-typescript
-	    lsp-terraform lsp-cpp))
+	(lsp-docker-init-clients
+	 :path-mappings '(("path-to-projects-you-want-to-use" . "/projects"))
+	 :client-packages lsp-docker-client-packages
+	 :client-configs lsp-docker-client-configs)
 
-(defvar lsp-docker-client-configs
-  (list
-   (list :server-id 'bash-ls :docker-server-id 'bashls-docker :server-command "bash-language-server start")
-   (list :server-id 'clangd :docker-server-id 'clangd-docker :server-command "ccls")
-   (list :server-id 'css-ls :docker-server-id 'cssls-docker :server-command "css-languageserver --stdio")
-   (list :server-id 'dockerfile-ls :docker-server-id 'dockerfilels-docker :server-command "docker-langserver --stdio")
-   (list :server-id 'gopls :docker-server-id 'gopls-docker :server-command "gopls")
-   (list :server-id 'html-ls :docker-server-id 'htmls-docker :server-command "html-languageserver --stdio")
-   (list :server-id 'pyls :docker-server-id 'pyls-docker :server-command "pyls")
-   (list :server-id 'ts-ls :docker-server-id 'tsls-docker :server-command "typescript-language-server --stdio")))
-
-(use-package lsp-docker)
-(lsp-docker-init-clients
- :path-mappings '(("path-to-projects-you-want-to-use" . "/projects"))
- :client-packages lsp-docker-client-packages
- :client-configs lsp-docker-client-configs)
+	(defvar lsp-docker-client-configs
+		(list
+		 (list :server-id 'bash-ls :docker-server-id 'bashls-docker :server-command "bash-language-server start")
+		 (list :server-id 'clangd :docker-server-id 'clangd-docker :server-command "ccls")
+		 (list :server-id 'css-ls :docker-server-id 'cssls-docker :server-command "css-languageserver --stdio")
+		 (list :server-id 'dockerfile-ls :docker-server-id 'dockerfilels-docker :server-command "docker-langserver --stdio")
+		 (list :server-id 'gopls :docker-server-id 'gopls-docker :server-command "gopls")
+		 (list :server-id 'html-ls :docker-server-id 'htmls-docker :server-command "html-languageserver --stdio")
+		 (list :server-id 'pyls :docker-server-id 'pyls-docker :server-command "pyls")
+		 (list :server-id 'ts-ls :docker-server-id 'tsls-docker :server-command "typescript-language-server --stdio"))))
 
 (provide '30-lsp)
 ;;; 30-lsp.el ends here
