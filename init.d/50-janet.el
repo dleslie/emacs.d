e2adaa5f090d67f5602adcc069e7bf02b4894370
Dan Leslie <dan@ironoxide.ca>
2023-05-02 18:19:27 -0700

    inf janet

--- init.d/50-janet.el
+++ init.d/50-janet.el
@@ -1,1 +1,8 @@
 (use-package janet-mode)
+(straight-use-package
+ '(inf-janet
+   :type git
+   :host github
+   :repo "velkyel/inf-janet"))
+(add-hook 'janet-mode-hook #'inf-janet-minor-mode)
+(add-hook 'janet-mode-hook #'paredit-mode)
