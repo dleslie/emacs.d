7fda7ca8fa930b3debd6f2605f01d5edc93ce4f2
Dan Leslie <dan@ironoxide.ca>
2023-05-05 11:14:58 -0700

    Updated org config

--- init.d/70-org.el
+++ init.d/70-org.el
@@ -7,20 +7,83 @@
 
 ;;; Code:
 
-(defvar org-directory '())
-
 (use-package org
   :defer t
+  :custom
+  (org-directory (file-truename "~/org/")
+   org-default-notes-file (concat (file-truename org-directory) "notes.org")
+   org-agenda-files `(,(concat (file-truename org-directory) "todo.org") ,(concat (file-truename org-directory) "agenda.org"))
+   org-agenda-diary-file (concat (expand-file-name org-directory) "diary.org"))
   :init
-  (global-set-key "\C-cot" 'org-todo-list)
-  (global-set-key "\C-col" 'org-store-link)
-  (global-set-key "\C-coa" 'org-agenda)
-  (global-set-key "\C-cos" 'org-iswitchb)
-  (global-set-key "\C-coc" 'org-capture)
-  (global-set-key "\C-coi" 'my-org-show-all-inline-images)
   (defun my-org-show-all-inline-images ()
     (interactive)
-    (org-display-inline-images t t)))
+    (org-display-inline-images t t))
+  (setq
+   org-todo-keywords
+   '((sequence "TODO(t)" "PROG(p)" "BLCK(b)" "STAL(s)" "|" "DONE(d)" "WONT(w)"))
+   org-todo-keyword-faces
+   '(("TODO" . (:foreground "white" :weight bold))
+     ("DOIN" . (:foreground "green" :weight bold))
+     ("BLCK" . (:foreground "red" :weight bold))
+     ("STAL" . (:foreground "yellow" :weight bold))
+     ("WONT" . (:foreground "grey" :weight bold))
+     ("DONE" . (:foreground "grey" :weight bold)))
+   org-capture-templates
+   '(("n" "Note" entry (file+headline "notes.org" "Notes")
+      "* %^{topic} %T %^g\n   :CATEGORY: %^{category}\n%i%?\n")
+     ("t" "To Do" entry (file+headline "todo.org" "To Do")
+      "* TODO %^{todo} %^g\n   DEADLINE: %^{due}t\n   :CATEGORY: %^{category}\n")
+     ("d" "Daily review" entry (file+headline "diary.org" "Daily Review")
+      (format
+       "* %%T %%^g\n   :CATEGORY: Review\n   %%?%%[%s/template_daily_review.org]\n"
+       org-directory))
+     ("i" "Idea" entry (file+headline "ideas.org" "Ideas")
+      "* %^{topic} %T %^g\n   :CATEGORY: Idea\n   %i%?\n")
+     ("j" "Journal" entry (file+headline "diary.org" "Journal")
+      "* %^{topic} %T %^g\n   :CATEGORY: Journal\n   %i%?\n")
+     ("l" "Letter" entry (file+headline "letters.org" "Letter")
+      "* %^{topic} %T %^g\n   :CATEGORY: Letter\n   %i%?\n")
+     ("w" "Work Log" entry (file+headline "work.org" "Work Log")
+      "* %^{topic} %T %^g\n   :CATEGORY: Log\n   %i%?\n")
+     ("a" "Article" entry (file+headline "articles.org" "Article")
+      "* %^{topic} %T %^g\n   :CATEGORY: Article\n   %i%?\n")
+     ("e" "Event" entry (file+headline "agenda.org" "Events")
+      "* %^{title} %^g\n     SCHEDULED: %^{when}t\n   %i%?\n")
+     ("c" "Contact" entry (file+headline "addresses.org" "Addresses")
+      "* %(org-contacts-template-name)\n   :PROPERTIES:\n   :EMAIL: %(org-contacts-template-email)\n   :END:\n   %i%?\n")))
+  :bind
+  (("C-c o c" . org-capture)
+   ("C-c o a" . org-agenda)
+   ("C-c o l" . org-store-link)
+   ("C-c o b" . org-iswitchb)
+   ("C-c o t" . org-todo-list)
+   ("C-c o s" . org-search-view)
+   ("C-c o i" . my-org-show-all-inline-images)))
+
+(use-package org-bullets
+  :after org
+  :init
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(defvar org-roam-directory '())
+
+(use-package org-roam
+  :after org
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory (file-truename "~/org/roam"))
+  :bind
+  (("C-c o r" . org-roam)
+   ("C-c o f" . org-roam-find-file)
+   ("C-c o g" . org-roam-graph))
+  :config
+  (setq org-roam-capture-templates
+	'(("d" "default" plain (function org-roam--capture-get-point)
+	   "%?"
+	   :file-name "%<%Y%m%d%H%M%S>-${slug}"
+	   :head "#+title: ${title}\n#+roam_tags: \n\n"
+	   :unnarrowed t))))
 
 (provide '70-org)
 ;;; 70-org.el ends here
--- local.el
+++ local.el
@@ -8,8 +8,6 @@
 ;;; Code:
 
 ;; Where are we storing org files?
-(defvar org-directory)
-(setq org-directory (expand-file-name "~/org/"))
 
 ;; Useful bindings
 (global-set-key "\C-w" 'clipboard-kill-region)
@@ -65,44 +63,6 @@
   :global t)
 
 (my-mode t)
-
-;; org stuff
-(setq
- org-default-notes-file (concat (expand-file-name org-directory) "notes.org")
- org-agenda-files `(,(concat (expand-file-name org-directory) "todo.org") ,(concat (expand-file-name org-directory) "agenda.org"))
- org-agenda-diary-file (concat (expand-file-name org-directory) "diary.org")
- org-todo-keywords
- '((sequence "TODO(t)" "PROG(p)" "BLCK(b)" "STAL(s)" "|" "DONE(d)" "WONT(w)"))
- org-todo-keyword-faces
- '(("TODO" . (:foreground "white" :weight bold))
-   ("DOIN" . (:foreground "green" :weight bold))
-   ("BLCK" . (:foreground "red" :weight bold))
-   ("STAL" . (:foreground "yellow" :weight bold))
-   ("WONT" . (:foreground "grey" :weight bold))
-   ("DONE" . (:foreground "grey" :weight bold)))
- org-capture-templates
- '(("n" "Note" entry (file+headline "notes.org" "Notes")
-    "* %^{topic} %T %^g\n   :CATEGORY: %^{category}\n%i%?\n")
-   ("t" "To Do" entry (file+headline "todo.org" "To Do")
-    "* TODO %^{todo} %^g\n   DEADLINE: %^{due}t\n   :CATEGORY: %^{category}\n")
-   ("d" "Daily review" entry (file+headline "diary.org" "Daily Review")
-    (format
-     "* %%T %%^g\n   :CATEGORY: Review\n   %%?%%[%s/template_daily_review.org]\n"
-     org-directory))
-   ("i" "Idea" entry (file+headline "ideas.org" "Ideas")
-    "* %^{topic} %T %^g\n   :CATEGORY: Idea\n   %i%?\n")
-   ("j" "Journal" entry (file+headline "diary.org" "Journal")
-    "* %^{topic} %T %^g\n   :CATEGORY: Journal\n   %i%?\n")
-   ("l" "Letter" entry (file+headline "letters.org" "Letter")
-    "* %^{topic} %T %^g\n   :CATEGORY: Letter\n   %i%?\n")
-   ("w" "Work Log" entry (file+headline "work.org" "Work Log")
-    "* %^{topic} %T %^g\n   :CATEGORY: Log\n   %i%?\n")
-   ("a" "Article" entry (file+headline "articles.org" "Article")
-    "* %^{topic} %T %^g\n   :CATEGORY: Article\n   %i%?\n")
-   ("e" "Event" entry (file+headline "agenda.org" "Events")
-    "* %^{title} %^g\n     SCHEDULED: %^{when}t\n   %i%?\n")
-   ("c" "Contact" entry (file+headline "addresses.org" "Addresses")
-    "* %(org-contacts-template-name)\n   :PROPERTIES:\n   :EMAIL: %(org-contacts-template-email)\n   :END:\n   %i%?\n")))
 
 ;; General Emacs Sanity
 (setq
