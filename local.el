39e8117e036c8efe827e1d42e4740423ba85647e
Dan Leslie <dan@ironoxide.ca>
2025-04-09 07:35:10 -0700

    Use a curated package set for treesitter.

--- local.el
+++ local.el
@@ -666,34 +666,11 @@ It will \"remember\" omit state across Dired buffers."
 
 (when (and (fboundp 'treesit-available-p)
            (treesit-available-p))
-  (when (or (not (boundp 'treesit-language-source-alist))
-            (not (listp treesit-language-source-alist)))
-    (setq treesit-language-source-alist '()))
-  ;; From combobulate: https://github.com/mickeynp/combobulate
-  ;; Added c/cpp/c# and rust
-  (dolist (grammar
-           '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
-             (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp" "v0.23.4"))
-             (c . ("https://github.com/tree-sitter/tree-sitter-c" "v0.23.5"))
-             (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.23.2"))
-             (c-sharp . ("https://github.com/tree-sitter/tree-sitter-c-sharp" "v0.23.1"))
-             (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
-             (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
-             (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
-             (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
-             (markdown . ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
-             (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
-             (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
-             (toml . ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
-             (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
-             (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
-             (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
-    (add-to-list 'treesit-language-source-alist grammar)
-    ;; Only install `grammar' if we don't already have it
-    ;; installed. However, if you want to *update* a grammar then
-    ;; this obviously prevents that from happening.
-    (unless (treesit-language-available-p (car grammar))
-      (treesit-install-language-grammar (car grammar))))
+
+  (use-package treesit-langs
+    :straight (:type git :host github :repo "emacs-tree-sitter/treesit-langs")
+    :config
+    (treesit-langs-major-mode-setup)))
 
   (if (eq 'windows-nt system-type)
       (add-to-list 'treesit-language-source-alist
@@ -703,22 +680,23 @@ It will \"remember\" omit state across Dired buffers."
   (when (not (treesit-language-available-p 'janet-simple))
     (treesit-install-language-grammar 'janet-simple))
 
-  (dolist (mapping
-           '((python-mode . python-ts-mode)
-             (c-mode . c-ts-mode)
-             (cpp-mode . cpp-ts-mode)
-             (css-mode . css-ts-mode)
-             (rust-mode . rust-ts-mode)
-             (csharp-mode . csharp-ts-mode)
-             (typescript-mode . typescript-ts-mode)
-             (js2-mode . js-ts-mode)
-             (bash-mode . bash-ts-mode)
-             (conf-toml-mode . toml-ts-mode)
-             (go-mode . go-ts-mode)
-             (css-mode . css-ts-mode)
-             (janet-mode . janet-ts-mode)
-             (json-mode . json-ts-mode)
-             (js-json-mode . json-ts-mode)))
+  (dolist
+      (mapping
+       '((python-mode . python-ts-mode)
+         (c-mode . c-ts-mode)
+         (cpp-mode . cpp-ts-mode)
+         (css-mode . css-ts-mode)
+         (rust-mode . rust-ts-mode)
+         (csharp-mode . csharp-ts-mode)
+         (typescript-mode . typescript-ts-mode)
+         (js2-mode . js-ts-mode)
+         (bash-mode . bash-ts-mode)
+         (conf-toml-mode . toml-ts-mode)
+         (go-mode . go-ts-mode)
+         (css-mode . css-ts-mode)
+         (janet-mode . janet-ts-mode)
+         (json-mode . json-ts-mode)
+         (js-json-mode . json-ts-mode)))
     (add-to-list 'major-mode-remap-alist mapping)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
